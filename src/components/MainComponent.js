1f1cea389221510f5fb4a6c213f459c646214548
amalmohann <amalmohann.com>
2020-10-17 21:13:22 +0530

    controlled forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,10 +6,12 @@ import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import DishDetail from './DishDetailComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
+
 
 
 class Main extends Component {
@@ -31,11 +33,18 @@ class Main extends Component {
                       leader={this.state.leaders.filter((leader)=>leader.featured)[0]} 
                       promotion={this.state.promotions.filter((promo)=>promo.featured)[0]}></Home>
             )
-        }
+        };
+
         const DishWithDetails = ({match}) => {
             return(
                 <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
                   comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+            );
+        };
+
+        const AboutUs = () => {
+            return(
+                <About leaders={this.state.leaders}/>
             );
         };
 
@@ -47,6 +56,7 @@ class Main extends Component {
                     <Route exact path='/Menu' component={() => <Menu dishes={this.state.dishes} />} />
                     <Route exact path='/Menu/:dishId' component={DishWithDetails} />
                     <Route exact path='/contactus' component={Contact}/>
+                    <Route exact path='/aboutus' component={AboutUs} />
                     <Redirect to='/home' ></Redirect>
                 </Switch>
                 <Footer />
